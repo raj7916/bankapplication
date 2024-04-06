24078a3e2d6959d00d7522389f16fe3db96c8f9d
raj <rajivbhadresa@outlookemail.ca>
2024-04-06 19:34:45 -0400

    2nd commit

---
+++ firstbank/src/firstbank/Firstbank.java
@@ -0,0 +1,17 @@
+package firstbank;
+
+import java.util.Scanner;
+public class Firstbank {
+
+	public static void main (String []args) {
+		
+		BankAccount obj1 = new BankAccount ("XYZ", "B234132");
+		obj1.showMenu();
+		
+		
+		
+	}
+	
+	
+	
+}
